44ceac9c1673e9a9d12f8a2a4595e060be72c08f
Robson Formigão <robson.formigao.gomes@hotmail.com>
2018-12-21 12:13:36 -0200

    initiating redux with an example counter

---
+++ counter/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+  return state;
+};
+
+console.assert(counter(0, { type: "INCREMENT" }) === 1);
+console.assert(counter(1, { type: "INCREMENT" }) === 2);
+
+console.assert(counter(2, { type: "DECREMENT" }) === 1);
+console.assert(counter(3, { type: "DECREMENT" }) === 2);
+
+console.assert(counter(3, { type: "SOMETHING" }) === 3);
+console.assert(counter(undefined, {}) === 0);
