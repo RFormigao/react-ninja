ab414bae8bb7f59189300d9b73e4f07a245976e2
Robson Formigão <robson.formigao.gomes@hotmail.com>
2018-12-27 03:08:42 -0200

    finish app todo list, :)

--- react-todo-list/src/components/todos-list/index.js
+++ react-todo-list/src/components/todos-list/index.js
@@ -3,10 +3,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleTodo } from "reducers/todos/action-creators";
+import * as filterActions from "reducers/visibility-filter/actions";
 
-const TodosList = ({ todos, handleToggleTodo }) => (
+const TodosList = ({ todos, activeFilter, handleToggleTodo }) => (
   <ul>
-    {todos.map(todo => (
+    {getVisibileTodos(todos, activeFilter).map(todo => (
       <li
         key={todo.id}
         style={{
@@ -21,8 +22,19 @@ const TodosList = ({ todos, handleToggleTodo }) => (
   </ul>
 );
 
+const getVisibileTodos = (todos, activeFilter) => {
+  const filterItems = {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter(todo => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter(todo => !todo.completed)
+  };
+
+  return filterItems[activeFilter];
+};
+
 const mapStateToProps = state => ({
-  todos: state.todos
+  todos: state.todos,
+  activeFilter: state.visibilityFilter
 });
 
 const mapDispatchToProps = dispatch => ({
