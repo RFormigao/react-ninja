c901925b6bc2b508d7ec811859e360f9fb1a0a13
Robson Formigão <robson.formigao.gomes@hotmail.com>
2019-03-03 21:09:13 -0300

    structure of the project react-flix

--- react-flix/src/app.js
+++ react-flix/src/app.js
@@ -1,43 +1,53 @@
 "use strict";
 
-import React, { PureComponent } from "react";
+import React, { Fragment } from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
 import "normalize.css";
 import "milligram";
 
-class App extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      title: "...",
-      Component: "div"
-    };
-  }
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Container>
+      <Header>
+        <h1>Reactflix</h1>
+      </Header>
+      <Main>Contéudo</Main>
+      <Footer>&copy; 2018</Footer>
+    </Container>
+  </Fragment>
+);
 
-  getTitle() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("My app with async / await!");
-      }, 2000);
-    });
+const GlobalStyle = createGlobalStyle`
+  html, body, div[data-js="app"] {
+    height: 100%
   }
+`;
 
-  async componentDidMount() {
-    const title = await import("components/title");
+const Container = styled.div`
+  height: 100%;
+`;
 
-    this.setState({
-      title: await this.getTitle(),
-      Component: title.default
-    });
-  }
+const Header = styled.header`
+  background-color: #262626;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+`;
 
-  render() {
-    return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
-      </div>
-    );
-  }
-}
+const Main = styled.main`
+  min-height: calc(100% - 120px);
+`;
+
+const Footer = styled.footer`
+  background-color: #333;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default App;
